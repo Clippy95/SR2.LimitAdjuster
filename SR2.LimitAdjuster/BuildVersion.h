befa0831bf479b1ae3151cf9df6acfb9e1c902b0
Clippy95 <qpzezoqp15@gmail.com>
2026-05-29 01:06:21 +0300

    ignore it now

--- SR2.LimitAdjuster/BuildVersion.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace BuildVersion
-{
-    inline constexpr char kBuildTimestamp[] = "2026-05-29 01:05:46 +03:00";
-    inline constexpr unsigned int kGitCommitCount = 10;
-    inline constexpr char kGitShortHash[] = "6a80fe0";
-    inline constexpr bool kGitDirty = false;
-}
